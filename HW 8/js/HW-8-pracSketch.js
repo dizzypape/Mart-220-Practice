1fe9e132e3e36b8a26f731b1f84037e37b5908e2
Izzy Pape <pape.isabelle.44@gmail.com>
2023-03-24 18:02:44 -0600

    HW 8

--- HW 8/js/HW-8-pracSketch.js
+++ HW 8/js/HW-8-pracSketch.js
@@ -8,7 +8,8 @@ var oil;
 var healthBar = [];
 var i = 0;
 var points = 0;
-var wrench;
+var wrench = [];
+var ground = rect(0,590,width,10,'s');
 
 function preload() 
 {
@@ -20,34 +21,59 @@ function setup()
 {
     createCanvas(800,600);
 
-    gear[0] = createSprite(random(650, width),random(50, 245),100,100, 'static');
+    world.gravity.y = 10;
+
+    ground = createSprite(400,595,width,10,'s');
+
+    gear[0] = createSprite(random(650, 880),random(50, 525),100,100, 'static');
     gear[0].img = "./images/gear.png";
     gear[0].scale = 0.10;
     gear[0].diameter = 75;
 
-    gear[1] = createSprite(random(50, 285),random(255, 396),100,100, 'static');
+    gear[1] = createSprite(random(50, 150),random(50, 525),100,100, 'static');
     gear[1].img = "./images/gear.png";
     gear[1].scale = 0.10;
     gear[1].diameter = 75;
 
-    gear[2] = createSprite(random(315, 646),random(400, height),100,100, 'static');
+    gear[2] = createSprite(random(315, 550),random(50, 525),100,100, 'static');
     gear[2].img = "./images/gear.png";
     gear[2].scale = 0.10;
     gear[2].diameter = 75;
 
-    oil = createSprite(random(50, 200),random(400, 550), 100,100, 'd');
+    oil = createSprite(random(50, 200),random(400, 550), 100,100, 's');
     oil.img = "./images/oilbarrel.jpg";
     oil.scale = 0.10;
     oil.diameter = 0.5;
     oil.rotationLock = true;
 
-    robby = new robot(0, 0, 150, 150,'n');
-    robby.diameter = 75;
+    wrench[0] = createSprite(50,300,100,100,'d');
+    wrench[0].img = "./images/wrench.png";
+    wrench[0].scale = 0.10;
+    wrench[0].diameter = 5;
+    wrench[0].rotationLock = true;
+    wrench[0].bounciness = 1;
+
+    wrench[1] = createSprite(300,300,100,100,'d');
+    wrench[1].img = "./images/wrench.png";
+    wrench[1].scale = 0.10;
+    wrench[1].diameter = 5;
+    wrench[1].rotationLock = true;
+    wrench[1].bounciness = 1;
+
+    wrench[2] = createSprite(650,300,100,100,'d');
+    wrench[2].img = "./images/wrench.png";
+    wrench[2].scale = 0.10;
+    wrench[2].diameter = 5;
+    wrench[2].rotationLock = true;
+    wrench[2].bounciness = 1;
+
+    robby = new robot(0, 0, 150, 150);
+    robby.diameter = 100;
     robby.rotationLock = true;
     robby.loadAnimation('idle', idlePaths);
     robby.loadAnimation('walk', runPaths);
 
-    healthBar[i] = new healthbar(75,575,150,12,20,247,31);
+    healthBar[i] = new healthbar(75,575,160,12,20,247,31);
 }
 
 function draw()
@@ -123,9 +149,52 @@ function draw()
     if(robby.isColliding(oil))
     {
         oil.pos = {x: random(50, 750), y: random(50, 550)};
-        points += 5;
-        healthBar[i] = new healthbar(75,575,152,12,20,247,31);       
+        points += 2;
+        healthBar[i].w += 10;       
     }
+
+    if(robby.isColliding(wrench[0]))
+    {
+        points -= 2;
+        fill(236,247,0);
+        healthBar[i].w -= 20;
+    }
+    else if(robby.isColliding(wrench[1]))
+    {
+        points -= 2;
+        fill(236,247,0);
+        healthBar[i].w -= 20;
+    }
+    else if(robby.isColliding(wrench[2]))
+    {
+        points -= 2;
+        fill(236,247,0);
+        healthBar[i].w -= 20;
+    }
+
+
+    if(wrench[i].collided(ground))
+    {
+        wrench[i].vel.y = -12;
+    }
+
+    if(points == 10)
+    {
+        fill(255);
+        rect(0,0,800,600);
+        fill(0);
+        textSize(32);
+        text('You win!',350,250);
+    }
+    else if(healthBar[i].w <= 0)
+    {
+        fill(0);
+        rect(0,0,800,600);
+        fill(255);
+        textSize(32);
+        text('You died.',350,250);
+    }
+    
 }
 
 function createBoarder()
@@ -133,8 +202,6 @@ function createBoarder()
     //top
     fill(0);
     rect(0,0,width,10);
-    //bottom
-    rect(0,590,width,10);
     //left
     rect(0,0,10,height);
     //right
